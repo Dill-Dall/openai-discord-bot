355088ee74ab1e0684b3cf80d9314be421e344a3
DillDall <t.a.dahll@gmail.com>
2023-02-14 19:17:20 +0100

    Removed flask and nginx since not webfacing

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Python runtime as the base image
-FROM python:3.9-slim-buster
+FROM python:3.11-slim-buster
 
 # Set the working directory in the container to /app
 WORKDIR /app
@@ -8,18 +8,10 @@ WORKDIR /app
 COPY requirements.txt .
 
 # Install the Python packages listed in requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
 
 # Copy the rest of the app code to the container
 COPY . .
 
-RUN chown -R www-data:www-data /app/static
 
-# Set the environment variable for Flask
-ENV FLASK_APP=app.py
-
-# Expose port 5000 for the Flask app to listen on
-EXPOSE 5000
-
-# Run the Flask app when the container starts
-CMD ["flask", "run", "--host=0.0.0.0"]
+CMD ["flask", "app.py"]
--- app.py
+++ app.py
@@ -2,45 +2,11 @@ import os
 
 import discord
 import openai
-from flask import Flask, redirect, render_template, request, url_for
-
-app = Flask(__name__,
-            static_folder='static')
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
-
-
-# bot.py
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @client.event
