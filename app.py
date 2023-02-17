e3e278a4010bfa418c6715280f565d911563cec8
DillDall <t.a.dahll@gmail.com>
2023-02-18 00:28:49 +0100

    Features - Use of embeds - Use of "spinners"

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ from io import BytesIO
 import discord
 import openai
 
-import discord
 import logging
-
-discord.utils.setup_logging()
-
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 beer_number = os.getenv("BEER_NUMBER")
@@ -22,8 +18,8 @@ intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
 
-def doOpenAiQuestion(prompt, temperature=0.5, max_tokens=150, top_p=0.3, frequency_penalty=0.5,
-                     presence_penalty=0.0):
+def do_openai_question(prompt, temperature=0.5, max_tokens=150, top_p=0.3, frequency_penalty=0.5,
+                       presence_penalty=0.0):
 
     logging.info("PROMPT: {}".format(prompt))
     try:
@@ -39,15 +35,17 @@ def doOpenAiQuestion(prompt, temperature=0.5, max_tokens=150, top_p=0.3, frequen
 
     except openai.APIError as error:
         if error.status_code == 429:
-            return f">>> The billing limit has been reached. Please try again later. Else you can :beer: me @{admin_user} at {beer_number}"
-        return f">>> There was a server issue. Please try again later. @{admin_user}"
+            return f"The billing limit has been reached. Please try again later. Else you can :beer: me @{admin_user} at {beer_number}"
+        return f"There was a server issue. Please try again later. @{admin_user}"
 
-    return f">>> {response.choices[0].text.lstrip()}"
+    return f"{response.choices[0].text.lstrip()}"
 
 
-def generate_prompt(AiModel, inputs):
+def generate_prompt(ai_model, inputs):
+    if isinstance(inputs, str):
+        inputs = [inputs]
     formatted_inputs = [input.capitalize() for input in inputs]
-    return AiModel.value.format(*formatted_inputs)
+    return ai_model.value.format(*formatted_inputs)
 
 
 def weighted_random(lowest, highest, std_dev):
@@ -69,8 +67,19 @@ def glados_prompt():
 @client.event
 async def on_ready():
     logging.info('We have logged in as {0.user}'.format(client))
+    embed = discord.Embed(title='Booting...', color=discord.Color.blue())
     channel = client.get_channel(int(dev_channel))
-    await channel.send(doOpenAiQuestion(glados_prompt()))
+    working_message = await channel.send(embed=embed)
+    response = do_openai_question(glados_prompt())
+
+    embed.title = "Glados"
+    await embed_complete(embed, working_message, response)
+
+
+async def embed_complete(embed, working_message, response):
+    embed.color = discord.Color.green()
+    embed.description = response
+    await working_message.edit(embed=embed)
 
 
 @ client.event
@@ -78,35 +87,52 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hi'):
-        await message.channel.send('Hello!')
-    elif message.content.startswith('Timmy'):
-        question = message.content.replace('Timmy', '')
-        prompt = generate_prompt(AiModel.TIMMY, question)
-        response = doOpenAiQuestion(prompt)
-        await message.channel.send(response)
-    elif message.content.startswith('Glados'):
-        response = doOpenAiQuestion(glados_prompt())
-        await message.channel.send(response)
-    elif message.content.startswith('Glen'):
-        question = message.content.replace('Glen', '')
-        prompt = generate_prompt(AiModel.GLEN, question)
-        response = doOpenAiQuestion(prompt, temperature=0.3, max_tokens=int(weighted_random(12, 24, 6)), presence_penalty=0.2,
-                                    frequency_penalty=0.3)
-        await message.channel.send(response)
+    if any(message.content.startswith(prefix) for prefix in ['Timmy', 'Glados', 'Glen', 'Dall']):
+        embed = discord.Embed(title='Working...', color=discord.Color.blue())
+        working_message = await message.channel.send(embed=embed)
 
-    elif message.content.startswith('Dall'):
-        prompt = message.content.replace('Dall', '')
-        response = openai.Image.create(
-            prompt=prompt + " but as a cat",
-            n=1,
-            size="1024x1024"
-        )
-        image_url = response['data'][0]['url']
-        response = requests.get(image_url)
-        image_data = BytesIO(response.content)
+        if message.content.startswith('Timmy'):
+            question = message.content.replace('Timmy', '')
+            prompt = generate_prompt(AiModel.TIMMY, question)
+            response = do_openai_question(prompt)
+            embed.title = 'Timmy'
 
-        await message.channel.send(file=discord.File(image_data, 'image.png'))
+        elif message.content.startswith('Glados'):
+            response = do_openai_question(glados_prompt())
+            embed.title = 'Glados'
 
+        elif message.content.startswith('Glen'):
+            question = message.content.replace('Glen', '')
+            prompt = generate_prompt(AiModel.GLEN, question)
+            response = do_openai_question(
+                prompt,
+                temperature=0.3,
+                max_tokens=int(weighted_random(12, 24, 6)),
+                presence_penalty=0.2,
+                frequency_penalty=0.3
+            )
+            embed.title = 'Glen'
+
+        elif message.content.startswith('Dall'):
+            embed.set_image(
+                url='https://media.giphy.com/media/dtHz6tzJp3fbmopSwA/giphy.gif')
+            await working_message.edit(embed=embed)
+
+            prompt = message.content.replace('Dall', '')
+            response = openai.Image.create(
+                prompt=prompt,
+                n=1,
+                size="1024x1024"
+            )
+
+            image_url = response['data'][0]['url']
+            response = requests.get(image_url)
+            # image_data = BytesIO(response.content)
+
+            embed.set_image(url=image_url)
+            response = prompt
+            embed.title = 'Dall'
+
+    await embed_complete(embed, working_message, response)
 
 client.run(os.getenv("DISCORD_TOKEN"))
